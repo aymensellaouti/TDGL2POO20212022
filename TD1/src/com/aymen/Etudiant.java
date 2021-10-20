a797579165bca418be47a62cd99b5c4b440f4b5b
aymen2782 <aymen.sellaouti@gmail.com>
2021-10-20 09:10:02 +0100

    exercice 3 TD1

--- TD1/src/com/aymen/Etudiant.java
+++ TD1/src/com/aymen/Etudiant.java
@@ -1,4 +1,44 @@
 package com.aymen;
 
 public class Etudiant {
+    private String name;
+    private int[] notes;
+
+    public Etudiant(String name, int[] notes) {
+        this.name = name;
+        this.notes = notes;
+    }
+//            3)  Ecrire une méthode qui affiche les notes de chaque étudiant,
+    public void afficherNotes() {
+        for (int i = 0; i< notes.length; i++) {
+            System.out.println("note " + i + " = " + notes[i] );
+        }
+    }
+//4)  Ecrire une méthode qui calcule la moyenne,
+    public float moyenne() {
+        if (notes.length != 0) {
+            int somme = 0;
+            for (int i = 0; i < notes.length; i++) {
+                somme += notes[i];
+            }
+            float moyenne = somme / notes.length;
+            System.out.println("La moyenne de " + this.name + " = " + moyenne);
+            return moyenne;
+        }
+        System.out.println("La moyenne de " + this.name + " = 0");
+        return 0;
+    }
+
+//            5)  Ecrire une méthode qui affiche si l’étudiant est « admis » ou « non admis ».
+    public void estAdmis() {
+        if (moyenne() >= 10) {
+            System.out.println("Admis");
+        } else {
+            System.out.println("non admis");
+        }
+    }
+//            6)  Ecrire la méthode main permettant de calculer la moyenne et d’afficher le résultat pour les deux
+//    étudiants suivants :
+//            -  Ali, 11, 13, 18, 7
+//            -  Faten, 15, 9, 8, 16
 }
--- TD1/src/com/aymen/Main.java
+++ TD1/src/com/aymen/Main.java
@@ -5,14 +5,22 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-        Vecteur v1 = new Vecteur(3);
-        Vecteur v2 = new Vecteur(7,9);
-        //  afficher les deux vecteurs
-        v1.afficher();
-        v2.afficher();
-        // Todo : calculer et afficher le produit scalaire
-        System.out.println("Le produit scalaire est égale à : " + v1.produitScalaire(v2));
+        int[] t = new int[0];
+        //Exercice 3
+        //            -  Ali, 11, 13, 18, 7
+        //            -  Faten, 15, 9, 8, 16
+        Etudiant ali = new Etudiant("Ali", new int[]{11, 13, 18, 7});
+        Etudiant faten = new Etudiant("Faten", new int[]{15, 9, 8, 16});
+        ali.estAdmis();
+        faten.estAdmis();
+        //Exercice 2
+//        Vecteur v1 = new Vecteur(3);
+//        Vecteur v2 = new Vecteur(7,9);
+//        //  afficher les deux vecteurs
+//        v1.afficher();
+//        v2.afficher();
+//        // Todo : calculer et afficher le produit scalaire
+//        System.out.println("Le produit scalaire est égale à : " + v1.produitScalaire(v2));
         //        Scanner scanner = new Scanner(System.in);
 //        int longeur = scanner.nextInt();
 //        int largeur = scanner.nextInt();
