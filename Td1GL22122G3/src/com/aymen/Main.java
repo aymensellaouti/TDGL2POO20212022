f203201c26737a315613e1ee304bab6a57c35854
aymen2782 <aymen.sellaouti@gmail.com>
2021-10-20 10:31:47 +0100

    TD1 part 2 exercice 2

--- Td1GL22122G3/src/com/aymen/Main.java
+++ Td1GL22122G3/src/com/aymen/Main.java
@@ -1,11 +1,22 @@
 package com.aymen;
 
+import java.awt.*;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-//        First a =  new First(1);
+
+        /*
+        * Ecrire la fonction main ( ) permettant d’initialiser deux vecteurs v1 et v2, d’afficher l’abscisse et
+        l’ordonnée de chacun, de calculer leurs produits scalaires et de l’afficher.
+        * */
+        Vecteur vecteur1 = new Vecteur(4);
+        Vecteur vecteur2 = new Vecteur(7,23);
+        vecteur1.afficher();
+        vecteur2.afficher();
+        System.out.println("le produit scalaire est :" + vecteur1.produitScalaire(vecteur2));
+        //        First a =  new First(1);
 //        First b = a;
 //        System.out.println(a.equals(b));
 //        b =  new First(1);
